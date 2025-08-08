7ffa5e4805d63cad66a469fc8dc03a68ed554aa3
codingisfuk <wwb8524@naver.com>
2025-08-08 11:53:48 +0900

    SERVLET

--- src/main/java/org/example/k4_iot_servlet/db/DBConnection.java
+++ src/main/java/org/example/k4_iot_servlet/db/DBConnection.java
@@ -1,5 +1,9 @@
 package org.example.k4_iot_servlet.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/k4_iot_servlet";
     private static final String JDBC_USER = "root";
@@ -9,7 +13,11 @@ public class DBConnection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            e.fillInStackTrace();
+            e.printStackTrace();
         }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
     }
 }
